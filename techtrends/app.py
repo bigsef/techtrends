90e43a82a09efdcc672fb6d21365733c008770a5
Mahmud Youssef <mahmud.youssef@icloud.com>
2021-08-08 09:35:11 +0200

    fix reviewing comments

--- techtrends/app.py
+++ techtrends/app.py
@@ -138,5 +138,5 @@ def metrics():
 
 # start the application on port 3111
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
     app.run(host='0.0.0.0', port='3111')
